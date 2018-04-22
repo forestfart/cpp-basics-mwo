28dba1b99fa84a61c6ef3a4c86a769e35a081c40
Michal Michalik <michalmi@d17-l428-pc-02.ipa.iisg.agh.edu.pl>
2018-04-22 14:43:05 +0200

    zadanie na kartkowke

--- proj2/main.cpp
+++ proj2/main.cpp
@@ -2,19 +2,19 @@
 
 using namespace std;
 
-void swap2Ints(int &x, int &y)
+void swap2Ints(int *pA, int *pB)
 {
-    int wsk = x;
-    x = y;
-    y = wsk;
-}
+    int wsk = *pA;
+    *pA = *pB;
+    *pB = wsk;
+} // na kartkowce bedzie trzeba opisac co bedzie wynikiem dzialania roznych wersji
 
 int main()
 {
     int x = 1, y = 2;
 
     cout << "x = " << x << ", y = " << y << endl;
-    swap2Ints(x, y);
+    swap2Ints(&x, &y);
     cout << "x = " << x << ", y = " << y << endl;
 
     return 0;
